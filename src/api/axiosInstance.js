9752b923b714f1c618dc8938ab9ac2c9e4a671b7
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-20 00:15:47 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://shoewebback.onrender.com/",
+  baseURL: "https://shoewebback.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {
